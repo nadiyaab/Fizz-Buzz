8eb209e1ce86017df1ec8dae48d0d88128fe5525
Nadiya <nadiya.abdi@zoopla.co.uk>
2020-07-07 17:39:46 +0100

    saved files

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fizz_Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            for(var counter = 1; counter < 100; counter++) 
+            {
+               var output = "";
+
+               if(counter % 3 == 0)
+               {
+                   output += "Fizz";
+               }
+               
+               if(output == "")
+               {
+                   Console.WriteLine(counter);
+               }
+               else
+               {
+                   Console.WriteLine(output);
+               }
+            }
+            
+        }
+    }
+}
